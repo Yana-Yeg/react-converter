c87f0fe60b174ae41b56868d62690c686d6b39f0
Yana Yegorina <89519566+Yana-Yeg@users.noreply.github.com>
2022-05-25 08:56:25 +0200

    redux

--- src/redux/counterSlice.js
+++ src/redux/counterSlice.js
@@ -1,10 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { getCurrentExchangeRate } from "./counterOperations";
+import { getCurrentExchangeRate, getCount } from "./counterOperations";
 
 const counterSlice = createSlice({
   name: "counter",
   initialState: {
     counterResult: [],
+    countRes: "",
     isLoading: false,
     error: null,
   },
@@ -16,10 +17,25 @@ const counterSlice = createSlice({
     }),
     [getCurrentExchangeRate.fulfilled]: (state, { payload }) => ({
       ...state,
-      counterResult: [payload],
+      counterResult: payload,
       isLoading: false,
     }),
     [getCurrentExchangeRate.rejected]: (state, { payload }) => ({
+      ...state,
+      isLoading: false,
+      error: payload,
+    }),
+    [getCount.pending]: (state) => ({
+      ...state,
+      isLoading: true,
+      error: null,
+    }),
+    [getCount.fulfilled]: (state, { payload }) => ({
+      ...state,
+      countRes: payload,
+      isLoading: false,
+    }),
+    [getCount.rejected]: (state, { payload }) => ({
       ...state,
       isLoading: false,
       error: payload,
--- src/utils/someFetch.js
+++ src/utils/someFetch.js
@@ -1,11 +1,24 @@
 import axios from "axios";
 const BASE_URL = "https://api.apilayer.com";
-const API_KEY = "UZL513SXWudjJ2MA5yHr5Kiu6VlYNpLU";
+const API_KEY = "RUpWPJfW8D70iZG1Ae1Ic5M9rCCEY2Nn";
 
-export default async function fetchGetCurrent(symbol) {
+export async function fetchGetCurrent(symbol) {
   try {
     const response = await axios.get(
-      `${BASE_URL}/exchangerates_data/latest?symbols=${symbol}&base=UAH&apikey=${API_KEY}`
+      `${BASE_URL}/exchangerates_data/latest?symbols=USD%2C%20EUR&base=UAH&apikey=${API_KEY}`
+    );
+    // console.log(response.data.rates);
+    return response.data.rates;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function fetchGetCount(values) {
+  try {
+    const { optionFrom, optionTo } = values;
+    const response = await axios.get(
+      `${BASE_URL}/exchangerates_data/latest?symbols=${optionFrom}&base=${optionTo}&apikey=${API_KEY}`
     );
     console.log(response.data.rates);
     return response.data.rates;
